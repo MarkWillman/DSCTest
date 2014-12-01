8d0d3c49e6649b3b8d81c378f2108b234c75a177
MARKW-PULL <MARKW-PULL@localhost.local>
2014-11-30 22:45:31 -0600

    MARKW-PULL pushing PullServerInfo.ps1

---
+++ PullServerInfo.ps1
@@ -0,0 +1,9 @@
+$pullServerInfo = @{
+"pullServerName" = "MARKW-PULL"
+"pullServerPrivateIp" = "10.176.133.50"
+"pullServerPublicIp" = "119.9.52.122"
+"region" = "syd"
+"isRackConnect" = $true
+"isManaged" = $false
+"defaultRegion" = "IAD"
+}
